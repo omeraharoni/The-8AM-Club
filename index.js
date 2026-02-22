66eadbc493863c1da2ef4171b8704e7a618b0e42
ArielGuralnick99 <arielgur@post.bgu.ac.il>
2026-02-22 18:20:01 +0200

    Lock Express to v4 and update wildcard syntax for deployment stability

--- index.js
+++ index.js
@@ -288,7 +288,7 @@ app.post('/api/invitations/:id/respond', authenticateToken, async (req, res) => 
 app.use(express.static(path.join(__dirname, 'client/dist')));
 
 // For any route that doesn't match an API route, send back the index.html
-app.get('/*', (req, res) => {
+app.get('(.*)', (req, res) => {
     res.sendFile(path.join(__dirname, 'client/dist', 'index.html'));
 });
 
