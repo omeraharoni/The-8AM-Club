df7abcce84251d75064e307dd3b4ba961b3fdd2e
ArielGuralnick99 <arielgur@post.bgu.ac.il>
2026-02-24 19:11:44 +0200

    Add mobile debugger and lower JS target to ES2020

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,6 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/',
   plugins: [react()],
+  build: {
+    target: 'es2020'
+  },
   server: {
     host: '0.0.0.0',
     port: 5173,
