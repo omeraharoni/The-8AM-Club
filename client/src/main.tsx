9f08c194b5c2cc1597bcbf77e67b7f6fd2606f39
ArielGuralnick99 <arielgur@post.bgu.ac.il>
2026-02-24 19:45:06 +0200

    Fix TS1484 type-only import for ReactNode

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,4 +1,4 @@
-import { StrictMode, Component, ReactNode } from 'react'
+import { StrictMode, Component, type ReactNode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
